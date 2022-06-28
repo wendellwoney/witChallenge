a100b5f122a814a1352f09bcf8c5ade5b584fa57
Wendell <wendell@ibtech.inf.br>
2022-06-28 15:58:58 -0300

    Add Operation id

--- rest/src/main/java/com/wendellwoney/Model/ResponseModel.java
+++ rest/src/main/java/com/wendellwoney/Model/ResponseModel.java
@@ -1,18 +1,25 @@
 package com.wendellwoney.Model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResponseModel {
     private boolean hasError;
     private String message;
+    private String operationId;
 
     public ResponseModel(boolean hasError, String message) {
         this.hasError = hasError;
         this.message = message;
     }
 
+    public ResponseModel(boolean hasError, String operationId, String message) {
+        this.hasError = hasError;
+        this.message = message;
+        this.operationId = operationId;
+    }
 }
