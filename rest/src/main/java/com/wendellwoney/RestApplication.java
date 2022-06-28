b00c55ff49717cb49d282dc66ac9a6305233bc10
Wendell <wendell@ibtech.inf.br>
2022-06-28 15:16:27 -0300

    change to work ExceptionIfNoHandlerFound

--- rest/src/main/java/com/wendellwoney/RestApplication.java
+++ rest/src/main/java/com/wendellwoney/RestApplication.java
@@ -1,11 +1,28 @@
 package com.wendellwoney;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
+@EnableWebMvc
 public class RestApplication {
+    @Autowired
+    private DispatcherServlet servlet;
     public static void main(String[] args) {
         SpringApplication.run(RestApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner getCommandLineRunner(ApplicationContext context) {
+        servlet.setThrowExceptionIfNoHandlerFound(true);
+        return args -> {};
     }
 }
