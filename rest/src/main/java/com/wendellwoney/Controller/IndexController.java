e4e87fec485663e1b6e369ae5ce633e968ff5d96
Wendell <wendell@ibtech.inf.br>
2022-06-28 15:42:42 -0300

    Call Service

--- rest/src/main/java/com/wendellwoney/Controller/IndexController.java
+++ rest/src/main/java/com/wendellwoney/Controller/IndexController.java
@@ -1,18 +1,52 @@
 package com.wendellwoney.Controller;
 
 import com.wendellwoney.Model.ResponseModel;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.wendellwoney.Service.IIndexService;
+import com.wendellwoney.Service.IndexService;
+import com.wendellwoney.Service.OperationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class IndexController implements IRestController {
+public class IndexController implements IIndexController {
     @Override
     public ResponseEntity<ResponseModel> sum(Double valueOne, Double valueTwo) {
         try {
-            return ResponseEntity.ok(new ResponseModel(false,"Request Sent"));
-        } catch (Exception e) {
+            IIndexService service =  new IndexService();
+            return ResponseEntity.ok(service.sum(valueOne, valueTwo));
+        } catch (OperationException e) {
             return ResponseEntity.badRequest().body(new ResponseModel(true, e.getMessage()));
         }
     }
+
+    @Override
+    public ResponseEntity<ResponseModel> minus(Double valueOne, Double valueTwo) {
+        try {
+            IIndexService service =  new IndexService();
+            return ResponseEntity.ok(service.minus(valueOne,valueTwo));
+        } catch (OperationException e) {
+            return ResponseEntity.badRequest().body(new ResponseModel(true, e.getMessage()));
+        }
+    }
+
+    @Override
+    public ResponseEntity<ResponseModel> mutiply(Double valueOne, Double valueTwo) {
+        try {
+            IIndexService service =  new IndexService();
+            return ResponseEntity.ok(service.mutiply(valueOne,valueTwo));
+        } catch (OperationException e) {
+            return ResponseEntity.badRequest().body(new ResponseModel(true, e.getMessage()));
+        }
+    }
+
+    @Override
+    public ResponseEntity<ResponseModel> division(Double valueOne, Double valueTwo) {
+        try {
+            IIndexService service =  new IndexService();
+            return ResponseEntity.ok(service.division(valueOne, valueTwo));
+        } catch (OperationException e) {
+            return ResponseEntity.badRequest().body(new ResponseModel(true, e.getMessage()));
+        }
+    }
+
 }
