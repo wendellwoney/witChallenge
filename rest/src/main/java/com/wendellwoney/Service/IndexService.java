310e9ee8d4a7dbb8ea2bff67a92b8c073c3143f4
Wendell <wendell@ibtech.inf.br>
2022-06-28 15:59:26 -0300

    WIP service rest

--- rest/src/main/java/com/wendellwoney/Service/IndexService.java
+++ rest/src/main/java/com/wendellwoney/Service/IndexService.java
@@ -1,12 +1,15 @@
 package com.wendellwoney.Service;
 
 import com.wendellwoney.Model.ResponseModel;
+import com.wendellwoney.Tool;
 
 public class IndexService implements IIndexService{
     @Override
     public ResponseModel sum(Double valueOne, Double valueTwo) throws OperationException {
         try {
-            return new ResponseModel(false,"Request Sum Sent");
+            String uuid = Tool.uuid();
+
+            return new ResponseModel(false, uuid, "Request sum sent");
         } catch (Exception e) {
             throw new OperationException("Erro to send operation sun!");
         }
@@ -15,7 +18,9 @@ public class IndexService implements IIndexService{
     @Override
     public ResponseModel minus(Double valueOne, Double valueTwo) throws OperationException {
         try {
-            return new ResponseModel(false,"Request Sum Sent");
+            String uuid = Tool.uuid();
+
+            return new ResponseModel(false, uuid, "Request minus sent");
         } catch (Exception e) {
             throw new OperationException("Erro to send operation minus!");
         }
@@ -24,7 +29,9 @@ public class IndexService implements IIndexService{
     @Override
     public ResponseModel mutiply(Double valueOne, Double valueTwo) throws OperationException {
         try {
-            return new ResponseModel(false,"Request Sum Sent");
+            String uuid = Tool.uuid();
+
+            return new ResponseModel(false, uuid, "Request mutiply sent");
         } catch (Exception e) {
             throw new OperationException("Erro to send operation mutiply!");
         }
@@ -33,7 +40,9 @@ public class IndexService implements IIndexService{
     @Override
     public ResponseModel division(Double valueOne, Double valueTwo) throws OperationException {
         try {
-            return new ResponseModel(false,"Request Sum Sent");
+            String uuid = Tool.uuid();
+
+            return new ResponseModel(false, uuid, "Request division sent");
         } catch (Exception e) {
             throw new OperationException("Erro to send operation division!");
         }
