c7eaa8d51666578456cf796bb921ed50bab6808f
Wendell <wendell@ibtech.inf.br>
2022-06-29 23:55:11 -0300

    add logger info console

--- queue/src/main/java/com/wendellwoney/queue/Service/QueueService.java
+++ queue/src/main/java/com/wendellwoney/queue/Service/QueueService.java
@@ -1,18 +1,24 @@
 package com.wendellwoney.queue.Service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+
+import java.util.Locale;
 
 @Service
 public class QueueService implements IQueueService {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
     public void sender(String queue, Object message, String uuid) {
 
+        logger.info("Sendin rabbitmq: Exchange " +  rabbitTemplate.getConnectionFactory().getPort() + " Queue " + queue + " Uuid " + uuid);
         this.rabbitTemplate.convertAndSend(queue, message, m -> {
             m.getMessageProperties().setHeader("uuid", uuid);
             return m;
